7d988c4875b7865c76650a8fa8233e9ba0327c50
Drizzle <i@drizzle.vip>
2021-02-14 11:57:08 +0800

    修正

--- game/index.php
+++ game/index.php
@@ -3,5 +3,8 @@ $db = new Mysql();
 $user=$_SESSION['userid'];
 $game_name = $db->table('role')->field('name')->where("Id=$user")->item();
 ?>
-个人信息：<br>
-角色昵称：<?php echo $game_name['name']; ?>
+<div style="font-size: 20px;margin-top: 30px;position: absolute"></div>
+<div style="text-align: left;color: white;font-size: 14px">
+    个人信息：<br>
+    角色昵称：<?php echo $game_name['name']; ?>
+</div>
--- lib/login.php
+++ lib/login.php
@@ -7,16 +7,13 @@ $id = $_POST['userid'];
 $password = $_POST['password'];
 require_once "mysql.php";
 $db = new Mysql();
-if (strpos($id, "@") != '') {
-    $res = $db->table('user')->field('*')->where("email=$id and password='$password'")->item();
-} else {
-    $res = $db->table('user')->field('*')->where("Id=$id and password='$password'")->item();
-}
+$res = $db->table('user')->field('*')->where("Id=$id and password='$password'")->item();
 if (!$res) {
     exit(json_encode(array('code' => 0, 'msg' => '登录失败')));
 }
 session_start();
-$_SESSION['userid'] = $res['Id'];
+
+$_SESSION['userid']=$id;
 exit(json_encode(array('code' => 1, 'msg' => '登录成功')));
 
 
